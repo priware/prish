a4e00343bce17f6056dd7f413cfe04679f68c156
Ali Shirvani <alishirv@pm.me>
2023-05-26 18:49:52 +0330

    static assets.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bytes::BytesMut;
 use futures_util::{SinkExt, StreamExt};
+use serde_derive::{Deserialize, Serialize};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_pty::AsyncPty;
 use tracing::instrument;
@@ -7,6 +8,12 @@ use tracing::{info, warn};
 use tracing_subscriber::FmtSubscriber;
 use warp::ws::{Message, WebSocket};
 use warp::Filter;
+
+#[derive(Deserialize, Serialize, Debug)]
+struct WinSize {
+    cols: u16,
+    rows: u16,
+}
 
 #[tokio::main]
 async fn main() {
@@ -16,17 +23,21 @@ async fn main() {
     tracing::subscriber::set_global_default(subscriber).expect("failed to set subscriber");
 
     let static_assets = warp::fs::dir("./static");
-    let terminal = warp::post().and(warp::path("terminals")).map(|| "110");
+    //    let terminal = warp::post().and(warp::path("terminals")).map(|| "110");
     let resize = warp::post()
-        .and(warp::path!("terminals" / String / "size"))
-        .map(|_size| info!("resize windows"));
+        .and(warp::path!("terminals"))
+        .and(warp::query::<WinSize>())
+        .map(|size| {
+            info!("resize windows {:?}", size);
+            "ok"
+        });
     let ws =
         warp::path!("terminals" / String)
             .and(warp::ws())
             .map(|_pid: String, ws: warp::ws::Ws| {
                 ws.on_upgrade(move |socket| client_connected(socket))
             });
-    let route = static_assets.or(terminal).or(ws);
+    let route = static_assets.or(resize).or(ws);
 
     warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
 }
@@ -38,6 +49,7 @@ async fn client_connected(ws: WebSocket) {
     let mut buf = vec![0u8; 4096];
     let (mut ws_tx, mut ws_rx) = ws.split();
     let pty = AsyncPty::open().unwrap();
+    pty.resize(120, 32);
     let (mut pt_rx, mut pt_tx) = tokio::io::split(pty);
 
     loop {
